3a8bc589126ae862a4fff8410649f22ddcc1f57b
“Julio <Julio_Morales@epam.com>
2023-09-04 13:28:11 +0200

    homework4 -almost done

--- src/test/java/com/example/amazoncom/AmazonMainPage.java
+++ src/test/java/com/example/amazoncom/AmazonMainPage.java
@@ -1,16 +1,12 @@
 package com.example.amazoncom;
 
-import static org.testng.Assert.assertEquals;
-
 import java.time.Duration;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 public class AmazonMainPage {
 
   WebDriver driver;
@@ -19,6 +15,19 @@ public class AmazonMainPage {
 
   @FindBy(id = "nav-search-submit-button")
   WebElement searchButton;
+
+  @FindBy(id = "nav-cart")
+  WebElement linkCart;
+
+  @FindBy(css="a[class='a-text-center'][2]")
+  WebElement linkDaysOffer;
+
+  @FindBy(css="div[class=\"a-section maple-banner__text\"]")
+  WebElement textAmazonRecargas;
+
+  @FindBy( css="h2")
+  WebElement emptyCartMessage;
+
   final static String URL_WEB_AMAZON = "https://www.amazon.es/";
 
   public AmazonMainPage(WebDriver driver) {
